38bace51941632a4223db277cb0b56efe44ceaec
josephochiengodongo <josephodongo99@gmail.com>
2025-11-02 01:37:09 +0300

    effect

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,9 +1,5 @@
-
-   <script>
-        const lastModified = new Date(document.lastModified);
-        const days=string(lastModified.getDate()).padStart (2,'0');
-        const months=string(lastModified.getMonth()+1).padStart (2,'0');
-        const years=lastModified.getFullYear();
-        const formattedDate=`Last Modified: ${days}/${months}/${years}`;
-        document.getElementById('lastmod').textContent = formattedDate;
-   </script>
+<p id="lastmod"></p>
+<script>
+    const lastModified = new Date(document.lastModified);
+    const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' };
+</script>
