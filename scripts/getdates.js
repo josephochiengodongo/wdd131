2fb5e965efc1dfe24f6592b5cc58d5834e5b14de
josephochiengodongo <josephodongo99@gmail.com>
2025-11-02 02:05:29 +0300

    effect

--- scripts/getdates.js
+++
@@ -1,5 +0,0 @@
-<p id="lastmod"></p>
-<script>
-    const lastModified = new Date(document.lastModified);
-    const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' };
-</script>
