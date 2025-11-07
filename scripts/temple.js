a40001b72bd9632f936fdaf6d8aea55d68f92fc6
josephochiengodongo <josephodongo99@gmail.com>
2025-11-08 02:46:49 +0300

    effect

--- scripts/temple.js
+++ scripts/temple.js
@@ -1,4 +1,4 @@
 const mainnav = document.querySelector('navigation');
-const tembutton = document.querySelector('#menu');
-tembutton.addEventListener('click', ()=>{mainnav.classList.toggle('show');tembutton.classList.toggle('show');});
+const temButton = document.querySelector('#menu');
+temButton.addEventListener('click', ()=>{mainnav.classList.toggle('open');temButton.classList.toggle('open');});
 
