b7afdd6f7fcf4782d56d616f1d934a80ace8a293
josephochiengodongo <josephodongo99@gmail.com>
2025-11-09 01:44:46 +0300

    effect

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,10 +1,16 @@
 const navigation=document.querySelector('.navigation');
 const temButton=document.querySelector('#menu');
+
 temButton.addEventListener('click', ()=>{
     navigation.classList.toggle('open');
     temButton.classList.toggle('open');
 })
+const menuToggle=document.getElementById("mobile-menu");
+const navLink=document.getElementById("navlink");
 
+menuTongle.addEventListener("click", ()=>{
+navLink.classList.toggle    
+});
 
 
 
