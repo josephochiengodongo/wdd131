e612228fea7b88aaa7dfc5f77c5916cab027d772
josephochiengodongo <josephodongo99@gmail.com>
2025-11-24 19:51:19 +0300

    effect

--- scripts/filtered.js
+++ scripts/filtered.js
@@ -91,4 +91,41 @@ const temples = [
   },
   
 ];
+createTempCard();
+
+function createTempCard() {
+    temples.forEach(temple=>{
+        let card=document.createElement("section");
+        let name=document.createElement("h3");
+        let location=document.createElement("p");
+        let dedication=document.createElement("p");
+        let area =document.createElement("p");
+        let img=document.createElement("img");
+
+        name.textContent=temple.templeName;
+        location.innerHTML='<span class="label">Location:</span> $(temple.location)';
+        dedication.innerHTML='<span class="label">Dedicated:</span> $(temple.dedication)';
+        area.innerHTML='<span clas="label">size:</span> $(temple.area) sq ft';
+        img.setAttribute("src",temple.imageUrl);
+        img.setAttribute("alt",'$(temple.templeName)temple');
+        img.setAttribute("loading","lazy");
+
+
+        card.appendChild(name);
+        card.appendChild(location);
+        card.appendChild(dedication);
+        card.appendChild(area);
+        card.appendChild(img);
+
+
+    
+
+
+        
+    })
+}
+
+
+
+
 
