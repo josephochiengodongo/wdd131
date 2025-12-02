850c0a7df29034b2c55acabb7b2f0e6ca7807833
josephochiengodongo <josephodongo99@gmail.com>
2025-12-02 17:03:29 +0300

    effect

--- scripts/form.js
+++ scripts/form.js
@@ -5,3 +5,10 @@ hambutton.addEventListener('click', () => {
 	mainnav.classList.toggle('show');
 	hambutton.classList.toggle('show');
 });
+
+function showLastModified() {
+    const lastModified = new Date(document.lastModified);
+    const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' };
+    const formattedDate = lastModified.toLocaleDateString('en-US', options);
+    document.getElementById('lastModified').textContent = `Last Modified: ${formattedDate}`;
+}
